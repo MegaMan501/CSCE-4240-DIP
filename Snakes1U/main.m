5e6675b622f4913c3ba1609de149c086c9be901e
MegaMan501 <mohamed.rahaman501@gmail.com>
2019-01-31 13:32:49 -0600

    corrected issue with main

--- Snakes1U/main.m
+++ Snakes1U/main.m
@@ -1,14 +1,14 @@
 function main(f)
 
-p = "Enter file name: "; 
+p = 'Enter file name: '; 
 while (exist(f, 'file') == 0)
     f= input(p, 's'); 
 end
 
-np = input("Enter the number of segments: "); 
+np = input('Enter the number of segments: '); 
 while ( np > 25 ) || ( np < 10)
     disp('Enter a number between 10 and 25')
-    np = input("Enter the number of segments: "); 
+    np = input('Enter the number of segments: '); 
 end
 
 snake_manual_input(f, np, '-rs', 'redSquareContour');
